0c0ff2b1880ae9684f7687a49e2a43ff4bd3c75b
Jose Trosel <jtrosel.dev@gmail.com>
2023-12-19 21:42:56 -0400

    primer commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$variable = '';
+
+$resultado = 2 + 2;
+
+$variable2 = "hola vale, todo bien?";
+if (1 + 1 == 2) {
+    $variable2 = "hgola vale, todo mal?";
+    for ($i = 0; $i < 1; $i++) {
+        echo 'wasakaka';
+        if (1 - 1) {
+        }
+    }
+}
