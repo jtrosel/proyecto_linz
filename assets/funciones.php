59bf03d8ebaaac1c85f64a17c525a6ef2268c160
Jose Trosel <jtrosel.dev@gmail.com>
2023-12-19 22:38:54 -0400

    Segundo commit

---
+++ assets/funciones.php
@@ -0,0 +1,5 @@
+<?php
+function funcion_determinado()
+{
+    echo "hola";
+}
--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 <?php
+require('./assets/funciones.php');
+$variable2 = "hola vale, todo bien?";
 $variable = '';
-
 $resultado = 2 + 2;
 
-$variable2 = "hola vale, todo bien?";
 if (1 + 1 == 2) {
-    $variable2 = "hgola vale, todo mal?";
+    $variable2 = "?";
     for ($i = 0; $i < 1; $i++) {
         echo 'wasakaka';
         if (1 - 1) {
         }
     }
 }
+
+
+funcion_determinado();
